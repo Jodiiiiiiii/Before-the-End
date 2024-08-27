741a0fd346bf49802a85dc27394c5459c1cd0361
Jodi Foster <33043348+JohhnSmithh@users.noreply.github.com>
2024-08-27 18:23:08 -0400

    Removed Unused Import

--- Assets/Scripts/PanelBehavior/PanelControls.cs
+++ Assets/Scripts/PanelBehavior/PanelControls.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 /// <summary>
 /// Handles inputs related to clicking and dragging panels to move them.
