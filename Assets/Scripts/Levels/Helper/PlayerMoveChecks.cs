b02114a126822392fbec3477d392dc631bb270a4
Jodi Foster <fosterjv111@gmail.com>
2024-10-01 12:21:16 -0400

    PlayerMoveChecks refactor
    Each type of movement had its logic extracted to separate sub-functions, including for panel pushing, moving into logs, water, tunnels, and clocks.
    
    All mechanics seem to work still

--- Assets/Scripts/Levels/Helper/PlayerMoveChecks.cs
+++ Assets/Scripts/Levels/Helper/PlayerMoveChecks.cs
@@ -11,23 +11,22 @@ public static class PlayerMoveChecks
     /// </summary>
     public static void TryPlayerMove(PlayerControls player, Vector2Int moveDir)
     {
-        #region PRECONDITION VALIDATION
-        // Ensure target position is validly only one unit away
+        // REQUIREMENT: moveDir is unit vector
         if (moveDir.magnitude != 1 || (moveDir.x != 1 && moveDir.x != -1 && moveDir.x != 0) || (moveDir.y != 1 && moveDir.y != -1 && moveDir.y != 0))
             throw new Exception("Input of CanMove function MUST have only one non-zero value and it must be eiether -1 or 1.");
 
-        if (!player.TryGetComponent(out Mover playerObjMover))
+        // REQUIREMENT: Player has Mover component
+        if (!player.TryGetComponent(out Mover playerMover))
             throw new Exception("Player object MUST have Mover component.");
 
-        Vector2Int currPos = playerObjMover.GetGlobalGridPos();
+        Vector2Int currPos = playerMover.GetGlobalGridPos();
         Vector2Int targetPos = currPos + moveDir;
         
-        // Ensure player current visibility
+        // REQUIREMENT: Player is visible on current panel
         if (!VisibilityChecks.IsVisible(player, currPos.x, currPos.y))
             return;
-        #endregion
 
-        #region SPRITE FLIPPING
+        // SPRITE FLIPPING
         bool hasFlipped = false;
         int prevFrame = UndoHandler.GetGlobalFrame();
         if (player.IsFacingRight() && moveDir == Vector2Int.left)
@@ -40,154 +39,67 @@ public static class PlayerMoveChecks
             player.SetFacingRight(true);
             hasFlipped = true;
         }
-        #endregion
 
         // try all panel pushing and moving w/ objects action possibilities
         // ensure save stack frame happens in finally section if hasFlipped is true
         try
         {
-            #region PANEL PUSHING
-            // Check if adjacent/target pos is in a different panel
+            // PANEL PUSHING
             if (!VisibilityChecks.IsVisible(player, targetPos.x, targetPos.y))
             {
                 // Attempt to panel push
                 TryPanelPush(player, targetPos, moveDir);
-
                 // Player has either pushed the panel OR no change has occurred. Move action is over.
                 return;
             }
-            #endregion
 
-            #region OBJECTS / PLAYER MOVEMENT
-            // Check for object in current panel at target position
-            QuantumState objState = VisibilityChecks.GetObjectAtPos(playerObjMover, targetPos.x, targetPos.y);
-            if (objState is null)
+            // NO OBSTRUCTION
+            QuantumState adjacentObj = VisibilityChecks.GetObjectAtPos(playerMover, targetPos.x, targetPos.y);
+            if (adjacentObj is null)
             {
-                // Move action complete
-                ConfirmPlayerMove(playerObjMover, moveDir);
+                // Move action complete -> no obstruction
+                ConfirmPlayerMove(playerMover, moveDir);
                 return;
             }
 
-            // Player collides immediately with WHICH object type??
-            switch(objState.ObjData.ObjType)
+            // HANDLE SPECIFIC OBSTRUCTION
+            switch(adjacentObj.ObjData.ObjType)
             {
                 case ObjectType.Log:
-
-                    // push logs as possible
-                    if(PushLogsInSeries(playerObjMover, playerObjMover.GetGlobalGridPos() + moveDir, moveDir))
-                        ConfirmPlayerMove(playerObjMover, moveDir); // move if logs were actually pushed
-
+                    TryMoveIntoLog(playerMover, moveDir);
                     // action attempt is complete whether move happened or not
                     return;
                 case ObjectType.Water:
-                    // allow movement if water has an object in it
-                    if(objState.ObjData.WaterHasLog || objState.ObjData.WaterHasRock)
-                    {
-                        // Move action complete
-                        ConfirmPlayerMove(playerObjMover, moveDir);
-                        return;
-                    }
-
+                    TryMoveIntoWater(playerMover, moveDir, adjacentObj);
+                    // action attempt is complete whether move happened or not
                     return;
                 case ObjectType.Rock:
                     return; // CANNOT move into rocks
                 case ObjectType.TallRock:
                     return; // CANNOT move into tall rocks
                 case ObjectType.Bush:
-                    return; // unimplemented
+                    return; // CANNOT move into bushes
                 case ObjectType.TallBush:
-                    return; // unimplemented
+                    return; // CANNOT move into tall bushes
                 case ObjectType.Tunnel:
-                    
-                    // require the player to be moving UP into the tunnel (other directions are just an obstruction)
-                    if (moveDir != Vector2Int.up)
-                        return;
-
-                    // retrieve mover component of other tunnel
-                    QuantumState otherTunnel = objState.ObjData.OtherTunnel;
-                    if (!otherTunnel.TryGetComponent(out Mover otherTunnelMover))
-                        throw new Exception("Level Objects MUST have a mover component.");
-                    
-                    // Requirement: tunnel is visible
-                    Vector2Int otherTunnelPos = otherTunnelMover.GetGlobalGridPos();
-                    if (!VisibilityChecks.IsVisible(otherTunnel.gameObject, otherTunnelPos.x, otherTunnelPos.y))
-                        return;
-                    
-                    // Requirement: visibility of exit pos
-                    Vector2Int exitPos = otherTunnelPos + Vector2Int.down;
-                    if (!VisibilityChecks.IsVisible(otherTunnel.gameObject, exitPos.x, exitPos.y))
-                        return;
-
-                    // Requirement: exit pos is clear of obstructions (anything)
-                    QuantumState exitObj = VisibilityChecks.GetObjectAtPos(otherTunnelMover, exitPos.x, exitPos.y);
-                    if (exitObj is not null)
-                        return;
-                    
-                    // ALL PRECONDITIONS ARE MET -> player can move
-
-                    // Check for log sinking before moving through tunnel
-                    QuantumState logSinkCheck = VisibilityChecks.GetObjectAtPos(playerObjMover, currPos.x, currPos.y);
-                    if (logSinkCheck != null && logSinkCheck.ObjData.ObjType == ObjectType.Water && logSinkCheck.ObjData.WaterHasLog)
-                        logSinkCheck.ObjData.WaterHasLog = false;
-
-                    // move player to panel of the other tunnel
-                    player.transform.parent = otherTunnel.transform.parent;
-
-                    // move player to position beneath other tunnel
-                    playerObjMover.SetGlobalGoal(exitPos.x, exitPos.y);
-
-                    // visually flip the player
-                    PlayerSpriteSwapper spriteSwapper = player.GetComponentInChildren<PlayerSpriteSwapper>();
-                    if (spriteSwapper is null)
-                        throw new Exception("Player MUST have PlayerSpriteSwapper component.");
-                    spriteSwapper.RequireFlip();
-
-                    // completed player movement action
-                    UndoHandler.SaveFrame();
-                    
+                    TryMoveIntoTunnel(playerMover, moveDir, adjacentObj);
+                    // action attempt is complete whether move happened or not
                     return;
                 case ObjectType.Clock:
-                    // confirm move FIRST
-                    ConfirmPlayerMove(playerObjMover, moveDir);
-                    // then make call for transitioning level
-                    // TODO: will be replaced instead later with:
-                    // 1. updating completed level state in game manager
-                    // 2. transition BACK to level select at pos. indicated by current compelted level
-                    SceneTransitionHelper.LoadNextScene();
-
-                    return; // unimplemented
+                    TryMoveIntoClock(playerMover, moveDir);
+                    // action attempt is complete (and the entire level!)
+                    return;
             }
-            #endregion
         }
         finally
         {
-            // check if flip happened but new undo frame has not been saved yet
+            // SPRITE FLIP SAVE FRAME (if not already saved)
             if (hasFlipped && prevFrame == UndoHandler.GetGlobalFrame())
                 UndoHandler.SaveFrame();
         }
-
-        throw new Exception("Issue with TryPlayerMove. Should have returned at some point but did not.");
     }
 
-    /// <summary>
-    /// Contains all necessary functions when player move is confirmed
-    /// Handles potential log sinking (on curr position), player movement, and saving an undo frame.
-    /// </summary>
-    private static void ConfirmPlayerMove(Mover objMover, Vector2Int moveDir)
-    {
-        // If player was on log, sink log along with player movement
-        Vector2Int currPos = objMover.GetGlobalGridPos();
-        QuantumState logSinkCheck = VisibilityChecks.GetObjectAtPos(objMover, currPos.x, currPos.y);
-        if (logSinkCheck != null && logSinkCheck.ObjData.ObjType == ObjectType.Water && logSinkCheck.ObjData.WaterHasLog)
-            logSinkCheck.ObjData.WaterHasLog = false;
-
-        // move player
-        objMover.Increment(moveDir);
-
-        // completed player movement action
-        UndoHandler.SaveFrame();
-    }
-
+    #region MOVEMENT TYPES
     /// <summary>
     /// Attempts to push (in moveDir direction) by one position the topmost panel located at targetPos.
     /// OR attempts to push current panel (in moveDir direction) - if adjacent panel is of lower order.
@@ -260,12 +172,130 @@ public static class PlayerMoveChecks
     }
 
     /// <summary>
+    /// Attempts to push logs in series, moving the player.
+    /// </summary>
+    private static void TryMoveIntoLog(Mover playerMover, Vector2Int moveDir)
+    {
+        // push logs as possible
+        if (PushLogsInSeries(playerMover, playerMover.GetGlobalGridPos() + moveDir, moveDir))
+            ConfirmPlayerMove(playerMover, moveDir); // move if logs were actually pushed
+    }
+
+    /// <summary>
+    /// Attempts to move onto water tile. Only possible if water has a log or a rock in it.
+    /// </summary>
+    private static void TryMoveIntoWater(Mover playerMover, Vector2Int moveDir, QuantumState adjacentWater)
+    {
+        // allow movement if water has an object in it
+        if (adjacentWater.ObjData.WaterHasLog || adjacentWater.ObjData.WaterHasRock)
+        {
+            // Move action complete
+            ConfirmPlayerMove(playerMover, moveDir);
+            return;
+        }
+
+        // TODO: account for baryonyx swimming state
+    }
+
+    /// <summary>
+    /// Attempts to move into tunnel. Only possible if moving up into tunnel and other tunnel is visible and clear of obstructions.
+    /// </summary>
+    private static void TryMoveIntoTunnel(Mover playerMover, Vector2Int moveDir, QuantumState adjacentTunnel)
+    {
+        // PRECONDITION REQUIREMENTS
+
+        // require the player to be moving UP into the tunnel (other directions are just an obstruction)
+        if (moveDir != Vector2Int.up)
+            return;
+
+        // retrieve mover component of other tunnel
+        QuantumState otherTunnel = adjacentTunnel.ObjData.OtherTunnel;
+        if (!otherTunnel.TryGetComponent(out Mover otherTunnelMover))
+            throw new Exception("Level Objects MUST have a mover component.");
+
+        // REQUIREMENT: tunnel is visible
+        Vector2Int otherTunnelPos = otherTunnelMover.GetGlobalGridPos();
+        if (!VisibilityChecks.IsVisible(otherTunnel.gameObject, otherTunnelPos.x, otherTunnelPos.y))
+            return;
+
+        // REQUIREMENT: visibility of exit pos
+        Vector2Int exitPos = otherTunnelPos + Vector2Int.down;
+        if (!VisibilityChecks.IsVisible(otherTunnel.gameObject, exitPos.x, exitPos.y))
+            return;
+
+        // REQUIREMENT: exit pos is clear of obstructions (anything)
+        QuantumState exitObj = VisibilityChecks.GetObjectAtPos(otherTunnelMover, exitPos.x, exitPos.y);
+        if (exitObj is not null)
+            return;
+
+        // ALL PRECONDITIONS ARE MET -> player can move
+
+        // Check for log sinking before moving through tunnel
+        Vector2Int currPlayerPos = playerMover.GetGlobalGridPos();
+        QuantumState logSinkCheck = VisibilityChecks.GetObjectAtPos(playerMover, currPlayerPos.x, currPlayerPos.y);
+        if (logSinkCheck != null && logSinkCheck.ObjData.ObjType == ObjectType.Water && logSinkCheck.ObjData.WaterHasLog)
+            logSinkCheck.ObjData.WaterHasLog = false;
+
+        // move player to panel of the other tunnel
+        playerMover.transform.parent = otherTunnel.transform.parent;
+
+        // move player to position beneath other tunnel
+        playerMover.SetGlobalGoal(exitPos.x, exitPos.y);
+
+        // visually flip the player
+        PlayerSpriteSwapper spriteSwapper = playerMover.GetComponentInChildren<PlayerSpriteSwapper>();
+        if (spriteSwapper is null)
+            throw new Exception("Player MUST have PlayerSpriteSwapper component.");
+        spriteSwapper.RequireFlip();
+
+        // completed player movement action
+        UndoHandler.SaveFrame();
+    }
+
+    /// <summary>
+    /// confirms movement of the player and then handles loads out of current level scene.
+    /// </summary>
+    private static void TryMoveIntoClock(Mover playerMover, Vector2Int moveDir)
+    {
+        // confirm move FIRST
+        ConfirmPlayerMove(playerMover, moveDir);
+
+        // then make call for transitioning level
+        // TODO: will be replaced instead later with:
+        // 1. updating completed level state in game manager
+        // 2. transition BACK to level select at pos. indicated by current compelted level
+        SceneTransitionHelper.LoadNextScene();
+    }
+    #endregion
+
+    #region OTHER HELPER FUNCTIONS
+    /// <summary>
+    /// Contains all necessary functions when player move is confirmed
+    /// Handles potential log sinking (on curr position), player movement, and saving an undo frame.
+    /// </summary>
+    private static void ConfirmPlayerMove(Mover objMover, Vector2Int moveDir)
+    {
+        // LOG SINKING (when player leaves floating log)
+        Vector2Int currPos = objMover.GetGlobalGridPos();
+        QuantumState logSinkCheck = VisibilityChecks.GetObjectAtPos(objMover, currPos.x, currPos.y);
+        if (logSinkCheck != null && logSinkCheck.ObjData.ObjType == ObjectType.Water && logSinkCheck.ObjData.WaterHasLog)
+            logSinkCheck.ObjData.WaterHasLog = false;
+
+        // move player
+        objMover.Increment(moveDir);
+
+        // completed player movement action
+        UndoHandler.SaveFrame();
+    }
+
+    /// <summary>
     /// Handles iterating through adjacent log objects to see how many in a series can be pushed. 
-    /// Accounts for interactions with water as necessary.
-    /// Includes bool crushEndLog so this can be reused during strong push ability.
-    /// Returns true/false whether a change in log states (push/crush) actually occurred).
+    /// Accounts for interactions with water/obstructions as necessary.
+    /// Does NOT handle saving undo frame (must be handled wherever this is called).
     /// </summary>
     /// <param name="mover">Any Mover component on the same panel as the logs being pushed.</param>
+    /// <param name="crushEndLog">Modifies logic to crush the log at the end in the case of an obstruction, rather than returning false.</param>
+    /// <returns>returns true/false whether a change in log states (push/crush) actually occurred.</returns>
     public static bool PushLogsInSeries(Mover mover, Vector2Int initialCheckPos, Vector2Int dir, bool crushEndLog = false)
     {
         // check for first log
@@ -323,7 +353,7 @@ public static class PlayerMoveChecks
                     logs[logs.Count - 1].ObjData.IsDisabled = true;
                 }
             }
-            else if(adjacentObj.ObjData.ObjType == ObjectType.Clock)
+            else if (adjacentObj.ObjData.ObjType == ObjectType.Clock)
             {
                 // exit loop
                 currIsLog = false;
@@ -336,7 +366,7 @@ public static class PlayerMoveChecks
                 break;
         }
 
-        if(!obstructed) // move logs normally
+        if (!obstructed) // move logs normally
         {
             // move logs
             foreach (QuantumState log in logs)
@@ -351,10 +381,10 @@ public static class PlayerMoveChecks
             // indicate logs have been pushed (original adjacentPos is now open)
             return true;
         }
-        else if(crushEndLog) // with obstruction, push then crush end log
+        else if (crushEndLog) // with obstruction, push then crush end log
         {
             // increment each log UNTIL the last
-            for(int i = 0; i < logs.Count-1; i++)
+            for (int i = 0; i < logs.Count - 1; i++)
             {
                 // increment each log
                 if (logs[i].TryGetComponent(out Mover logMover))
@@ -372,4 +402,5 @@ public static class PlayerMoveChecks
         // no actual change has occurred
         return false;
     }
+    #endregion
 }
