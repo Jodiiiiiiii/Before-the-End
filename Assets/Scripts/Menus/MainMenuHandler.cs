21b5369c1a1964ff58c6fc16a5047b8f6ddcc631
Jodi Foster <fosterjv111@gmail.com>
2025-01-27 15:26:53 -0500

    Main Menu: Quit Button Functionality

---
+++ Assets/Scripts/Menus/MainMenuHandler.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+/// <summary>
+/// Contains all button functionality and credits swapping located on the main menu UI scene.
+/// </summary>
+public class MainMenuHandler : MonoBehaviour
+{
+    private void Awake()
+    {
+        // configure whether resume button shows up at all
+    }
+
+    #region Main Menu Buttons
+    public void NewGameButton()
+    {
+
+    }
+
+    public void ResumeButton()
+    {
+
+    }
+
+    public void OptionsButton()
+    {
+
+    }
+
+    public void CreditsButton()
+    {
+
+    }
+
+    /// <summary>
+    /// Quit Button functionality.
+    /// Exits play mode (in editor) or closes application (in build)
+    /// </summary>
+    public void QuitButton()
+    {
+#if UNITY_EDITOR
+        // quits play mode if in editor
+        EditorApplication.ExitPlaymode();
+#endif
+        Application.Quit();
+    }
+    #endregion
+}
