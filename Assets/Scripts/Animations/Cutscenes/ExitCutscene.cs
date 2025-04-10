61d6f3760a17b005aa1bcf1dabc0d3aa417206d1
Jodi Foster <fosterjv111@gmail.com>
2025-04-10 13:38:09 -0400

    Cutscene: now exits properly to appropriate level select based on Resume functionality

--- Assets/Scripts/Animations/Cutscenes/ExitCutscene.cs
+++ Assets/Scripts/Animations/Cutscenes/ExitCutscene.cs
@@ -10,10 +10,12 @@ public class ExitCutscene : MonoBehaviour
 {
     [SerializeField, Tooltip("Scene name of level select to return to.")]
     private string _levelSelectName;
+    [SerializeField, Tooltip("Used to call functional scene transition.")]
+    private SceneTransitionHandler _transitionHandler;
 
     public void ReturnToLevelSelect()
     {
         // does not use scene transitions handler since cutscene already ends at full faded out color
-        SceneManager.LoadScene(_levelSelectName);
+        _transitionHandler.LoadScene(_levelSelectName);
     }
 }
