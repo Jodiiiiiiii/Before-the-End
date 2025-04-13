5aba90980fd32ba6433b74b5a65c06f87a6ab019
Jodi Foster <fosterjv111@gmail.com>
2025-04-13 11:48:25 -0400

    Cutscenes: if entering rewind cutscene with no undo binding, it will be reset to default controls FOR THE CUTSCENE ONLY - reset on exit to prevent duplicate bindings

--- Assets/Scripts/Animations/Cutscenes/CutsceneUndoPrompt.cs
+++ Assets/Scripts/Animations/Cutscenes/CutsceneUndoPrompt.cs
@@ -53,6 +53,8 @@ public class CutsceneUndoPrompt : MonoBehaviour
     [SerializeField, Tooltip("Used to read action bindings")]
     private InputActionReference _action;
 
+    private bool _actionReset = false; // by default, assume not reset
+
     /// <summary>
     /// Returns corresponding binding associated with action.
     /// Prioritizes first binding, and returns "Not Bound" if no bindiing found.
@@ -66,7 +68,43 @@ public class CutsceneUndoPrompt : MonoBehaviour
         else if (binding2 != "")
             return binding2;
         else
-            return "Unbound";
+            return ResetUndo();
+    }
+
+    public string ResetUndo()
+    {
+        // makes sure that bindings are properly UNBOUND after scene ends
+        _actionReset = true;
+
+        string actionToReset = _action.name;
+
+        // restore default behavior
+        InputSystem.actions.FindAction(actionToReset).RemoveAllBindingOverrides();
+
+        // return new reset binding string
+        return InputSystem.actions.FindAction(actionToReset).GetBindingDisplayString(0, InputBinding.DisplayStringOptions.DontIncludeInteractions);
+    }
+
+    private void OnDisable()
+    {
+        // re-unbind actions
+        // this is necessary to guarantee 100% that no duplicate bindings persist due to the manual overrides of this specific scene only
+        if (_actionReset)
+        {
+            string actionToUnbind = _action.name;
+
+            // restore override of unbound - primary binding
+            InputSystem.actions.FindAction(actionToUnbind).RemoveBindingOverride(0);
+            InputSystem.actions.FindAction(actionToUnbind).ApplyBindingOverride(0, "");
+
+            // restore override of unbound - secondary binding
+            InputSystem.actions.FindAction(actionToUnbind).RemoveBindingOverride(1);
+            InputSystem.actions.FindAction(actionToUnbind).ApplyBindingOverride(1, "");
+
+            // so it can actually be used again - if this wasn't here, it would break one control when the other was unbound
+            // not sure if this needs to stay here, but keeping it to be safe
+            InputSystem.actions.FindAction(actionToUnbind).Enable();
+        }
     }
     #endregion
 }
