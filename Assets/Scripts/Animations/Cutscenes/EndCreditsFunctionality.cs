ad3d2cb6bef2297e1123105d784d62b837bfb837
Jodi Foster <fosterjv111@gmail.com>
2025-08-08 14:21:46 -0400

    Credits: plays drums version of credits music only if player has 100% completed the game

--- Assets/Scripts/Animations/Cutscenes/EndCreditsFunctionality.cs
+++ Assets/Scripts/Animations/Cutscenes/EndCreditsFunctionality.cs
@@ -12,8 +12,17 @@ public class EndCreditsFunctionality : MonoBehaviour
 
     public void PlayCreditsMusic()
     {
+        // used to determine if completionist credits music should play instead
+        int count = 0;
+        foreach (string str in GameManager.Instance.SaveData.LevelsComplete)
+        {
+            char[] arr = str.ToCharArray();
+            if (arr.Length > 1 && (arr[0] == '1' || arr[0] == '2') && arr[1] == '-')
+                count++;
+        }
+
         // queue track
-        AudioManager.Instance.QueueCreditsMusic(false);
+        AudioManager.Instance.QueueCreditsMusic(count >= 70);
     }
 
     public void CutMusic()
